838713d06be5d4e9b6fb2a344882a90eeaf8a3a1
Pixailz <Pixailz1@gmail.com>
2022-06-04 11:25:34 +0200

    :fire: init/init_get_significant: - get index by index not anymore by value

--- src/init/init_get_significant.c
+++ src/init/init_get_significant.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_get_address.c                                 :+:      :+:    :+:   */
+/*   init_get_significant.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 01:26:42 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/06/01 02:04:58 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/06/04 11:23:58 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,15 +72,11 @@ int	get_most_significant_index(t_stack *s)
 	int		saved_number;
 
 	tmp = s;
-	saved_number = tmp->value;
 	saved_index = tmp->index;
 	while (tmp->next)
 	{
-		if (tmp->next->value > saved_number)
-		{
+		if (tmp->next->index > saved_index)
 			saved_index = tmp->next->index;
-			saved_number = tmp->next->value;
-		}
 		tmp = tmp->next;
 	}
 	return (saved_index);
