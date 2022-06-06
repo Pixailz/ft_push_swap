dfeaee0c06bf4d3cbd8028806a4e7b4275236c8b
Pixailz <pixailz1@gmail.com>
2022-06-06 22:12:24 +0200

    :package: algo/algo_4.c: - change index == 1 to index > next->index

--- src/algo/algo_4.c
+++ src/algo/algo_4.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 20:00:11 by pix               #+#    #+#             */
-/*   Updated: 2022/06/06 11:02:01 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/06/06 21:06:36 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	sort_2_last(t_push_swap *ps)
 {
-	if (ps->a->index == 1)
+	if (ps->a->index > ps->a->next->index)
 		sa(ps, 0);
 }
 
