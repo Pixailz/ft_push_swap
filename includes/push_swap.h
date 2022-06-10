faf4320ac8fa17f0c9d3b22492165ecde6ded9cb
Pixailz <Pixailz1@gmail.com>
2022-06-10 18:26:22 +0200

    :package: push_swap.h: - arranged proto

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:56:44 by brda-sil          #+#    #+#             */
-/*   Updated: 2022/06/08 19:47:21 by brda-sil         ###   ########.fr       */
+/*   Updated: 2022/06/10 18:12:34 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,6 @@ typedef struct s_push_swap
 	t_stack	*b;
 	int		length_a;
 	int		length_b;
-	int		is_stack_b_empty;
-	int		is_stack_a_empty;
 	int		most_significant_index;
 }				t_push_swap;
 
@@ -67,7 +65,10 @@ typedef struct s_push_swap
 
 // algo/algo_100.c
 void		algo_100(t_push_swap *ps);
-void		algo_100_sort_25(int median, t_push_swap *ps);
+int			algo_100_check(t_stack *s, int median);
+void		algo_100_push_median(t_push_swap *ps, int median);
+void		algo_100_sort_25(t_push_swap *ps, int min, int max);
+void		pb_less_significant_bewteen(t_push_swap *ps, int median);
 
 // algo/algo_10.c
 void		algo_10(t_push_swap *ps);
@@ -77,24 +78,24 @@ void		algo_2(t_push_swap *ps);
 
 // algo/algo_3.c
 void		algo_3(t_push_swap *ps);
+void		algo_3_part2(t_push_swap *ps);
 
 // algo/algo_4.c
 void		algo_4(t_push_swap *ps);
-void		sort_2_last(t_push_swap *ps);
 
 // algo/algo_5.c
 void		algo_5(t_push_swap *ps);
-void		sort_3_last(t_push_swap *ps);
 
 // algo/algo_utils.2.c
-int			is_sorted_not_good_a(t_push_swap *ps);
-int			is_sorted_not_good_a_check(int value, int next_value);
-void		move_stack(t_push_swap *ps, int less_significant_index);
+void		pa_all_b(t_push_swap *ps);
 void		pb_less_significant(t_push_swap *ps, int to_push);
+void		pb_most_significant(t_push_swap *ps, int to_push);
 
 // algo/algo_utils.c
 int			is_r_sorted(t_stack *s, int size);
 int			is_sorted(t_stack *s);
+int			is_sorted_not_good_a(t_push_swap *ps);
+int			is_sorted_not_good_a_check(int value, int next_value);
 
 // init/init.c
 void		init(t_push_swap *ps, int argc, char **argv);
@@ -115,19 +116,19 @@ void		post_pb(t_push_swap *ps);
 
 // ope/ra.c
 void		post_ra(t_push_swap *ps);
-void		ra(t_push_swap *ps, int is_rrr);
+void		ra(t_push_swap *ps, int is_rr);
 
 // ope/rb.c
 void		post_rb(t_push_swap *ps);
-void		rb(t_push_swap *ps, int is_rrr);
+void		rb(t_push_swap *ps, int is_rr);
 
 // ope/rra.c
 void		post_rra(t_push_swap *ps);
-void		rra(t_push_swap *ps, int is_rr);
+void		rra(t_push_swap *ps, int is_rrr);
 
 // ope/rrb.c
 void		post_rrb(t_push_swap *ps);
-void		rrb(t_push_swap *ps, int is_rr);
+void		rrb(t_push_swap *ps, int is_rrr);
 
 // ope/rr.c
 void		rr(t_push_swap *ps);
@@ -147,9 +148,9 @@ void		sb(t_push_swap *ps, int is_ss);
 void		ss(t_push_swap *ps);
 
 // parsing/parse.c
-t_stack		*parse(int argc, char **argv);
-t_stack		*parse_multiple_string(char **argv, int argc);
-t_stack		*parse_one_string(char *argv);
+void		parse(t_stack **a, int argc, char **argv);
+void		parse_multiple_string(t_stack **a, char **argv, int argc);
+void		parse_one_string(t_stack **a, char *argv);
 
 // parsing/parse_check.2.c
 int			parse_check_multiple_string_part2(char **tmp_list, int number_arg, \
@@ -178,32 +179,31 @@ int			main(int argc, char **argv);
 
 // stack/stack_free.c
 void		free_entry_point(t_push_swap *ps);
-void		stack_free(t_stack *s, int size);
+void		stack_free(t_stack *s);
 
 // stack/stack_utils.2.c
+void		stack_reverse_rotate(t_stack **s);
+void		stack_rotate(t_stack **s);
+void		stack_swap_top(t_stack **s);
+
+// stack/stack_utils.3.c
 int			stack_get_size(t_stack *s);
-int			stack_get_value_at_index(t_stack *s, int index);
 void		stack_view(t_push_swap *ps);
 void		stack_view_a(t_push_swap *ps);
 void		stack_view_b(t_push_swap *ps);
 
-// stack/stack_utils.3.c
-void		stack_arrange_index(t_stack *s);
-int			stack_get_value_at_top(t_stack *s);
-int			stack_get_value_before_top(t_stack *s);
-void		stack_pop_top_a(t_push_swap *ps);
-void		stack_pop_top_b(t_push_swap *ps);
-
 // stack/stack_utils.4.c
-int			get_index_at_top(t_stack *s);
+int			get_index_at_bottom(t_stack *s);
 int			get_pos_at_index(t_stack *s, int index);
+int			stack_get_value_at_bottom(t_stack *s);
+int			stack_get_value_before_bottom(t_stack *s);
 
 // stack/stack_utils.c
-t_stack		*stack_new(int content, int index);
-void		stack_new_a(int content, int index, t_push_swap *ps);
-void		stack_new_b(int content, int index, t_push_swap *ps);
-t_stack		*stack_push_bottom(t_stack *s, int value);
-void		stack_push_top(t_stack *s, int value, int index);
+void		stack_get_new(t_stack **s, int value, int index);
+void		stack_pop_bottom(t_stack **s);
+void		stack_pop_top(t_stack **s);
+void		stack_push_bottom(t_stack **s, int value, int index);
+void		stack_push_top(t_stack **s, int value, int index);
 
 /* ########################################################################## */
 
